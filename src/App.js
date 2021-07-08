96d024f9e206e4f83557a7a4b8a3270dcaa1e2c9
ashutosh <ashutoshkumar99055@gmail.com>
2021-07-08 21:07:35 +0530

    added sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Board from "./components/Board";
 
+import  "./styles/root.scss";
+
 const App = () => {
   return (
-    <>
-      <h1>TIC TAC TOE</h1>
+    <div className ="app">
+      <h1>TIC TAC TOE </h1>
       <Board/>
-    </>
+    </div>
   );
 };
 export default App;
